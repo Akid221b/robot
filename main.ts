43891c35a9bd573e76c6cf7e815470489f493e11
Akid221b <118974959+Akid221b@users.noreply.github.com>
2022-11-24 11:39:39 +0100

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+input.onGesture(Gesture.FreeFall, function () {
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 43)
+})
 basic.forever(function () {
-	
+    if (input.soundLevel() < 200) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 20)
+    } else {
+        maqueen.motorStop(maqueen.Motors.All)
+    }
 })
