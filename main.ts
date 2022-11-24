d9aab0667aea94209c6cadad174bea1bee981706
auxence92 <118936828+auxence92@users.noreply.github.com>
2022-11-24 14:29:41 +0100

    si bouton a presser le robot passe en mode suiveur de ligne

--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
+let message = 0
 basic.forever(function () {
+    message = 0
     if (20 < maqueen.Ultrasonic(PingUnit.Centimeters)) {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 100)
     }
@@ -16,17 +18,27 @@ basic.forever(function () {
             maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 10)
         }
     }
-    if (1 == maqueen.readPatrol(maqueen.Patrol.PatrolRight) && 1 == maqueen.readPatrol(maqueen.Patrol.PatrolLeft)) {
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 100)
+    if (input.buttonIsPressed(Button.A)) {
+        message += 1
+        while ((true as any) == (false as any)) {
+            break;
+        }
     }
-    if (0 == maqueen.readPatrol(maqueen.Patrol.PatrolRight) && 1 == maqueen.readPatrol(maqueen.Patrol.PatrolLeft)) {
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
-    }
-    if (1 == maqueen.readPatrol(maqueen.Patrol.PatrolRight) && 0 == maqueen.readPatrol(maqueen.Patrol.PatrolLeft)) {
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
-    }
-    if (0 == maqueen.readPatrol(maqueen.Patrol.PatrolRight) && 0 == maqueen.readPatrol(maqueen.Patrol.PatrolLeft)) {
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 20)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 20)
+})
+basic.forever(function () {
+    if (1 == message) {
+        if (1 == maqueen.readPatrol(maqueen.Patrol.PatrolRight) && 1 == maqueen.readPatrol(maqueen.Patrol.PatrolLeft)) {
+            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 100)
+        }
+        if (0 == maqueen.readPatrol(maqueen.Patrol.PatrolRight) && 1 == maqueen.readPatrol(maqueen.Patrol.PatrolLeft)) {
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+        }
+        if (1 == maqueen.readPatrol(maqueen.Patrol.PatrolRight) && 0 == maqueen.readPatrol(maqueen.Patrol.PatrolLeft)) {
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+        }
+        if (0 == maqueen.readPatrol(maqueen.Patrol.PatrolRight) && 0 == maqueen.readPatrol(maqueen.Patrol.PatrolLeft)) {
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 20)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 20)
+        }
     }
 })
