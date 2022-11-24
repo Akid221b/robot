eb2b478a15cf17e4779ebf13f6cff598a68bc29c
auxence92 <118936828+auxence92@users.noreply.github.com>
2022-11-24 14:40:26 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,31 +1,52 @@
 let message = 0
 basic.forever(function () {
-    message = 0
-    if (20 < maqueen.Ultrasonic(PingUnit.Centimeters)) {
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 100)
-    }
-    if (20 > maqueen.Ultrasonic(PingUnit.Centimeters) && 5 < maqueen.Ultrasonic(PingUnit.Centimeters)) {
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 20)
-    }
-    if (5 > maqueen.Ultrasonic(PingUnit.Centimeters)) {
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
-        basic.pause(1000)
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 10)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
-        basic.pause(1000)
-        if (5 > maqueen.Ultrasonic(PingUnit.Centimeters)) {
-            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 10)
-            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 10)
+    if (input.buttonIsPressed(Button.B)) {
+        message = 2
+        basic.showLeds(`
+            . . # # #
+            . # . . #
+            . . . # .
+            . . # . .
+            . # # # #
+            `)
+        while (input.buttonIsPressed(Button.B)) {
+            break;
         }
     }
     if (input.buttonIsPressed(Button.A)) {
-        message += 1
-        while ((true as any) == (false as any)) {
+        message = 1
+        while (input.buttonIsPressed(Button.A)) {
+            basic.showLeds(`
+                . . # . .
+                . # # . .
+                . . # . .
+                . . # . .
+                . # # # .
+                `)
             break;
         }
     }
 })
 basic.forever(function () {
+    if (2 == message) {
+        if (20 < maqueen.Ultrasonic(PingUnit.Centimeters)) {
+            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 100)
+        }
+        if (20 > maqueen.Ultrasonic(PingUnit.Centimeters) && 5 < maqueen.Ultrasonic(PingUnit.Centimeters)) {
+            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 20)
+        }
+        if (5 > maqueen.Ultrasonic(PingUnit.Centimeters)) {
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+            basic.pause(1000)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 10)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+            basic.pause(1000)
+            if (5 > maqueen.Ultrasonic(PingUnit.Centimeters)) {
+                maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 10)
+                maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 10)
+            }
+        }
+    }
     if (1 == message) {
         if (1 == maqueen.readPatrol(maqueen.Patrol.PatrolRight) && 1 == maqueen.readPatrol(maqueen.Patrol.PatrolLeft)) {
             maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 100)
