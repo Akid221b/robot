1c46db94f754bb6383d7e68636c682cb5ceb7103
auxence92 <118936828+auxence92@users.noreply.github.com>
2022-11-24 14:14:50 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 basic.forever(function () {
     if (20 < maqueen.Ultrasonic(PingUnit.Centimeters)) {
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 30)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 100)
+    }
+    if (20 > maqueen.Ultrasonic(PingUnit.Centimeters) && 5 < maqueen.Ultrasonic(PingUnit.Centimeters)) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 20)
     }
     if (5 > maqueen.Ultrasonic(PingUnit.Centimeters)) {
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
